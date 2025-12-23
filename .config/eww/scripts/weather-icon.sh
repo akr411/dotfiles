65b7de13440fd526590c2e124ea243f2f111a255
Anwaya Rai <rai.anwaya@gmail.com>
2025-12-23 19:14:06 +0530

    EWW visual improvements

--- .config/eww/scripts/weather-icon.sh
+++ .config/eww/scripts/weather-icon.sh
@@ -2,4 +2,4 @@
 
 source "$(dirname "$0")/fetch-weather.sh"
 
-fetch_with_cache "wttr.in/?format=%c" "$HOME/.cache/eww_weather_icon" "..." | head -n1
+fetch_with_cache "wttr.in/?format=%c" "$HOME/.cache/eww_weather_icon" "..." | head -n1 | xargs
